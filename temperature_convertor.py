cc2466df72f49e9752b6b2aa3c14dd8ca995635d
Tsotetsi <me@tsotetsithapeloo.co.za>
2021-05-06 12:54:45 +0200

    Added click events for buttons and entries.

--- temperature_convertor.py
+++ temperature_convertor.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import LabelFrame, Button, Entry
+from tkinter import LabelFrame, Button, Entry, END
 
 
 gui = tk.Tk()
@@ -20,11 +20,32 @@ celsius_label_frame.place(x=50, y=60, height=100, width=180)
 fahrenheit_label_frame.place(x=300, y=60, height=100, width=180)
 
 
+def clear_fields():
+    celsius_entry.delete(0, END)
+    fahrenheit_entry.delete(0, END)
+    results_entry.delete(0, END)
+
+
+def temperature_conversion():
+
+    if celsius_entry['state'] == "normal" and celsius_entry.get() != "":
+        to_fahrenheit = float(celsius_entry.get()) * 9/5 + 32
+        results_entry.delete(0, END)
+        results_entry.insert(0, to_fahrenheit)
+    elif fahrenheit_entry['state'] == "normal" and fahrenheit_entry.get() != "":
+        to_celsius = (float(fahrenheit_entry.get()) - 32) * 5/9
+        results_entry.delete(0, END)
+        results_entry.insert(0, to_celsius)
+    else:
+        return None
+
+
 def activate_celsius_entry():
     if celsius_entry['state'] == "normal":
         celsius_entry.config(state="disabled")
     else:
         celsius_entry.config(state="normal")
+        fahrenheit_entry.config(state="disabled")
 
 
 def activate_fahrenheit_entry():
@@ -32,6 +53,8 @@ def activate_fahrenheit_entry():
         fahrenheit_entry.config(state="disabled")
     else:
         fahrenheit_entry.config(state="normal")
+        celsius_entry.config(state="disabled")
+
 
 activate_celsius_btn = Button(gui, text="Activate C to F", command=activate_celsius_entry)
 activate_celsius_btn.place(x=80, y=170)
@@ -39,16 +62,17 @@ activate_celsius_btn.place(x=80, y=170)
 activate_fahrenheit_btn = Button(gui, text="Activate F to C", command=activate_fahrenheit_entry)
 activate_fahrenheit_btn.place(x=330, y=170)
 
-convert_button = Button(gui, text="Convert")
+convert_button = Button(gui, text="Convert", command=temperature_conversion)
 convert_button.place(x=130, y=240)
 
 results_entry = Entry(gui, bg="green", width=10)
-results_entry.place(x=220, y=240)
+results_entry.place(x=220, y=240, height=29.5)
 
-clear_button = Button(gui, text="Clear")
+clear_button = Button(gui, text="Clear", command=clear_fields)
 clear_button.place(x=340, y=240)
 
 exit_button = Button(gui, text="Exit", command=gui.destroy)
 exit_button.place(x=440, y=240)
+
 
 gui.mainloop()
